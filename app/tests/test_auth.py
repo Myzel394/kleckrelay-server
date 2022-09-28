6a7864d696b2a9c72a1868308acd26db104da140
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2022-09-28 21:39:55 +0200

    fixed testing path

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -1,15 +1,3 @@
-import json
-import uuid
-
-import pytest
-from starlette.testclient import TestClient
-
-from .helpers import create_item
-from app.models import Email
-from app.schemas.email import Email as EmailSchema
-from .test_database import override_get_db
-
-
 def test_can_create_account_with_valid_data(db, client):
     response = client.post(
         "/auth/signup",
