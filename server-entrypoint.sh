7fbfe7c75f3b91888f400f200b5261f1d01d478e
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-01-28 16:58:42 +0100

    improved & fixed email handler

--- server-entrypoint.sh
+++ server-entrypoint.sh
@@ -1,3 +1,3 @@
 echo "Starting KleckRelay API server" &&
 poetry run alembic upgrade head &&
-poetry run gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:80 app.main:app
+poetry run python email_handler.py & poetry run gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:80 app.main:app
--- setup-postfix.sh
+++ setup-postfix.sh
@@ -8,10 +8,12 @@ echo "Configuring basic Postfix config"
 postconf -e "inet_protocols = ipv4"
 postconf -e "inet_interfaces = all"
 postconf -e "maillog_file = /var/log/maillog"
-postconf -e "mydestination = localhost, ${MAIL_DOMAIN}, localhost.${MAIL_DOMAIN}"
 postconf -e "mydomain = ${MAIL_DOMAIN}"
 postconf -e "mynetworks = 127.0.0.0/8, [::1]/128"
 postconf -e "myorigin = ${MAIL_DOMAIN}"
+postconf -e "myhostname = ${APP_DOMAIN}"
+# Use Docker's built-in DNS server
+postconf -e "resolve_numeric_domain = yes"
 postconf -e "smtp_host_lookup = native,dns"
 #postconf -e "smtp_sasl_auth_enable = yes"
 #postconf -e "smtp_sasl_password_maps = hash:/etc/postfix/sasl_password"
@@ -19,21 +21,31 @@ postconf -e "smtp_host_lookup = native,dns"
 if [[ "${POSTFIX_USE_TLS,,}" =~ ^(yes|true|t|1|y)$ ]]; then
   postconf -e "smtp_use_tls = yes"
 fi
-echo "nameserver 1.1.1.1" > /var/spool/postfix/etc/resolv.conf
+echo "nameserver 1.1.1.1" > /var/spool/postfix/etc/resolv.conf TODO: Check
 echo "nameserver 1.1.1.1" > /etc/resolv.conf
 
 echo "Creating broken symlinks for Postfix"
 cp -f /etc/services /var/spool/postfix/etc/services
 
+echo "Configuring incoming mail handling for Postfix"
+cat > /etc/postfix/pgsql-relay-domains.cf<< EOF
+hosts = $(echo "$DB_URI" | sed 's/.*@\([^/]*\).*/\1/')
+user = $(echo "$DB_URI" | sed 's/postgresql:\/\///' | sed 's/@.*//' | cut -d':' -f1)
+password = $(echo "$DB_URI" | sed 's/postgresql:\/\///' | sed 's/@.*//' | cut -d':' -f2)
+dbname = $(echo "$DB_URI" | sed 's/.*\/\([^:]*\).*/\1/')
 
-
-
-cat >> /etc/postfix/transport<< EOF
-${MAIL_DOMAIN} smtp:kleckrelay-email:20381
+query = SELECT '%s' WHERE '%s' = '${MAIL_DOMAIN}' LIMIT 1;
 EOF
-postmap /etc/postfix/transport
-postconf -e "transport_maps = hash:/etc/postfix/transport"
-postconf -e "local_recipient_maps = @${MAIL_DOMAIN}"
+cat > /etc/postfix/pgsql-transport-maps.cf<< EOF
+hosts = $(echo "$DB_URI" | sed 's/.*@\([^/]*\).*/\1/')
+user = $(echo "$DB_URI" | sed 's/postgresql:\/\///' | sed 's/@.*//' | cut -d':' -f1)
+password = $(echo "$DB_URI" | sed 's/postgresql:\/\///' | sed 's/@.*//' | cut -d':' -f2)
+dbname = $(echo "$DB_URI" | sed 's/.*\/\([^:]*\).*/\1/')
+
+query = SELECT 'smtp:127.0.0.1:20381' WHERE '%s' = '${MAIL_DOMAIN}' LIMIT 1;
+EOF
+postconf -e "relay_domains = pgsql:/etc/postfix/pgsql-relay-domains.cf"
+postconf -e "transport_maps = pgsql:/etc/postfix/pgsql-transport-maps.cf"
 
 echo "Postfix configuration completed"
 echo "Starting Postfix"
