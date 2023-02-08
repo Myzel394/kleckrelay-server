76fb0fedaf5a6fe85f7e3e9033594e94edf906fa
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-02-08 19:44:51 +0100

    created global_settings.py model

--- app/models/global_settings.py
+++ app/models/global_settings.py
@@ -2,6 +2,7 @@ import sqlalchemy as sa
 
 from sqlalchemy.dialects.postgresql import UUID
 
+from app import default_life_constants
 from app.database.base import Base
 from ._mixins import IDMixin
 
@@ -10,18 +11,69 @@ __all__ = [
 ]
 
 
-class GlobalSettingsUser(Base, IDMixin):
-    __tablename__ = "m2m_global_settings_user"
+# All fields in this model must be named in the snake case version of the corresponding
+# `default_life_constants` field
+# Example: `ALLOW_STATISTICS` in `default_life_constants` must be named `allow_statistics` in
+# this model
+# All fields must be nullable as a `NULL` value means that the field should use the given
+# environment variable or the default value from `default_life_constants`
 
-    global_settings_id = sa.Column(UUID(as_uuid=True), ForeignKey("global_settings.id"))
-    user_id = sa.Column(UUID(as_uuid=True), ForeignKey("user.id"))
-
-
+# `ADMINS` will NOT be included in this model as being an admin should be read only
 class GlobalSettings(Base, IDMixin):
     __tablename__ = 'global_settings'
 
-    random_email_id_min_length = Column(
-
+    random_email_id_min_length = sa.Column(
+        sa.SmallInteger(),
+        default=None,
+        nullable=True,
     )
-
+    random_email_id_chars = sa.Column(
+        sa.String(),
+        max_length=1023,
+        default=None,
+        nullable=True,
+    )
+    random_email_length_increase_on_percentage = sa.Column(
+        sa.Float(),
+        default=None,
+        nullable=True,
+    )
+    custom_email_suffix_length = sa.Column(
+        sa.SmallInteger(),
+        default=None,
+        nullable=True,
+    )
+    custom_email_suffix_chars = sa.Column(
+        sa.String(),
+        max_length=1023,
+        default=None,
+        nullable=True,
+    )
+    image_proxy_storage_life_time_in_hours = sa.Column(
+        # A `SmallInteger` would limit the user to a maximum of about 3 years,
+        # but we want to allow the user to set a higher timeout.
+        sa.Integer(),
+        default=None,
+        nullable=True,
+    )
+    enable_image_proxy = sa.Column(
+        sa.Boolean(),
+        default=None,
+        nullable=True,
+    )
+    user_email_enable_disposable_emails = sa.Column(
+        sa.Boolean(),
+        default=None,
+        nullable=True,
+    )
+    user_email_enable_other_relays = sa.Column(
+        sa.Boolean(),
+        default=None,
+        nullable=True,
+    )
+    allow_statistics = sa.Column(
+        sa.Boolean(),
+        default=None,
+        nullable=True,
+    )
 
