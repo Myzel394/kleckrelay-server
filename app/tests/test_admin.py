ad85e802019c43f45952bf8c2b7185fcd3af5a5e
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-02-11 20:22:42 +0100

    test: Add no admin user test

--- app/tests/test_admin.py
+++ app/tests/test_admin.py
@@ -24,3 +24,19 @@ def test_admin_can_get_correct_admin_users(
     assert len(response.json()["users"]) == 1, "There should only be one admin user."
     assert str(response.json()["users"][0]["id"]) == str(user.id), \
         "Admin user should be in the response."
+
+
+def test_non_admin_can_not_access_admin_apis(
+    create_user,
+    create_auth_tokens,
+    client: TestClient,
+) -> None:
+    user = create_user(is_verified=True)
+    auth = create_auth_tokens(user)
+
+    response = client.get(
+        "/v1/admin/users/",
+        headers=auth["headers"],
+    )
+
+    assert response.status_code == 401, f"Status code should be 401 but is {response.status_code}"
