208d145d44ed7467742d37d5ef2a1b21945d42d0
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-02-17 16:32:36 +0100

    feat: Add CronReport model

---
+++ app/models/cron_report.py
@@ -0,0 +1,49 @@
+from typing import TYPE_CHECKING
+
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
+from app.database.base import Base
+from app.models import CreationMixin, IDMixin
+
+__all__ = [
+    "CronReportData",
+    "CronReport"
+]
+
+
+# This class contains the actual data of the report
+class CronReportData(Base, IDMixin):
+    if TYPE_CHECKING:
+        from .user import User
+        encrypted_report: str
+        user_id: str
+        user: User
+        report: "CronReport"
+        report_id: str
+    else:
+        encrypted_report = sa.Column(
+            sa.String(5_000),
+            nullable=False,
+        )
+        user_id = sa.Column(
+            UUID(as_uuid=True),
+            sa.ForeignKey("user.id"),
+        )
+        report_id = sa.Column(
+            UUID(as_uuid=True),
+            sa.ForeignKey("cron_report.id"),
+        )
+
+
+# This class contains the metadata of the report and includes multiple CronReportDatas
+class CronReport(Base, CreationMixin):
+    if TYPE_CHECKING:
+        data: list[CronReportData]
+    else:
+        data = relationship(
+            "CronReportData",
+            back_populates="report",
+        )
+
