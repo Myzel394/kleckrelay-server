2cdf6cb853b6e2023b9778b6c5bcde9114631ae0
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-02-19 21:09:16 +0100

    fix: Domain should be set automatically by browser

--- app/authentication/authentication_response.py
+++ app/authentication/authentication_response.py
@@ -17,7 +17,6 @@ def set_authentication_cookies(response: Response, user: User) -> None:
         value=access_security.create_access_token(subject=user.to_jwt_object()),
         httponly=not life_constants.IS_DEBUG,
         secure=not life_constants.IS_DEBUG,
-        domain=None if constants.IS_TESTING else life_constants.APP_DOMAIN,
         max_age=life_constants.ACCESS_TOKEN_EXPIRE_IN_MINUTES * 60,
     )
 
@@ -26,6 +25,5 @@ def set_authentication_cookies(response: Response, user: User) -> None:
         value=refresh_security.create_refresh_token(subject=user.to_jwt_object()),
         httponly=not life_constants.IS_DEBUG,
         secure=not life_constants.IS_DEBUG,
-        domain=None if constants.IS_TESTING else life_constants.APP_DOMAIN,
         max_age=life_constants.REFRESH_TOKEN_EXPIRE_IN_MINUTES * 60,
     )
