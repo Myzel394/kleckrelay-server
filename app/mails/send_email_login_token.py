855b6ab8f9d5d48d322362e51a90c112671733ff
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-02-27 10:50:37 +0100

    fix: Replace login email

--- app/mails/send_email_login_token.py
+++ app/mails/send_email_login_token.py
@@ -8,16 +8,16 @@ from email_utils.send_mail import send_template_mail
 def send_email_login_token(user: User, token: str) -> None:
     logger.info(f"Send email login token: Send email to {user.email.address}")
 
-    if IS_DEBUG:
-        logger.info(f"Send email login token: Token for {user.email.address} is {token}")
-
     send_template_mail(
-        template="email_login",
+        template="login",
         to=user.email.address,
         subject="Log in to KleckRelay",
         language=user.language,
         context={
-            "token": token,
-            "app_domain": life_constants.APP_DOMAIN,
+            "title": "Log in to KleckRelay",
+            "preview_text": "Here's your login code for KleckRelay",
+            "body": "Here's your login code for KleckRelay:",
+            "code": token,
+            "server_url": life_constants.APP_DOMAIN,
         }
     )
