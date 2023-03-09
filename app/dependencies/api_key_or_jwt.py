1a990d8c4c15328bafb1d898c5232e5742e281e7
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-03-09 22:41:31 +0100

    feat: Add scope handling to api_key_or_jwt dependency

--- app/dependencies/api_key_or_jwt.py
+++ app/dependencies/api_key_or_jwt.py
@@ -8,9 +8,14 @@ from starlette.requests import Request
 from app import constants
 from app.authentication.handler import access_security
 from app.controllers.api_key import find_api_key
+from app.models.enums.api_key import APIKeyScope
+
+__all__ = [
+    "api_key_or_jwt",
+]
 
 
-def extract_api_key_from_header(
+def _extract_api_key_from_header(
     request: Request
 ) -> Optional[str]:
     raw_header = request.headers.get("Authorization", "")
@@ -21,26 +26,35 @@ def extract_api_key_from_header(
     return None
 
 
-async def api_key_or_jwt(
-    db: Session,
-    api_key: Optional[str] = Depends(extract_api_key_from_header),
-    # Required for `access_security`
-    bearer: JwtAuthBase.JwtAccessBearer = Security(JwtAccess._bearer),
-    cookie: JwtAuthBase.JwtAccessCookie = Security(JwtAccess._cookie),
-):
-    if api_key is not None:
-        if (api_key_instance := find_api_key(db, api_key)) is not None:
-            return api_key_instance
+def api_key_or_jwt(scope: APIKeyScope):
+    async def _method(
+        db: Session,
+        api_key: Optional[str] = Depends(_extract_api_key_from_header),
+        # Required for `access_security`
+        bearer: JwtAuthBase.JwtAccessBearer = Security(JwtAccess._bearer),
+        cookie: JwtAuthBase.JwtAccessCookie = Security(JwtAccess._cookie),
+    ):
+        if api_key is not None:
+            if (api_key_instance := find_api_key(db, api_key)) is not None:
+                if scope in api_key_instance.scopes:
+                    return
 
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid API Key.",
-        )
-    else:
-        if await access_security(bearer=bearer, cookie=cookie):
-            return
+                raise HTTPException(
+                    status_code=401,
+                    detail="API Key does not have the required scope.",
+                )
 
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid JWT Token.",
-        )
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid API Key.",
+            )
+        else:
+            if await access_security(bearer=bearer, cookie=cookie):
+                return
+
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid JWT Token.",
+            )
+
+    return _method
