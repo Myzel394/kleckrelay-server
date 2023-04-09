9bb5c71de396fc4fe9ce5521b406c331cf1209be
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-04-09 11:58:34 +0200

    feat: Add email gpg public key verification to scheme

--- app/schemas/user_preferences.py
+++ app/schemas/user_preferences.py
@@ -1,5 +1,8 @@
-from pydantic import BaseModel, Field, root_validator
+from typing import Optional
 
+from pydantic import BaseModel, Field, root_validator, validator
+
+from app import constants, gpg_handler
 from app.models.enums.alias import ImageProxyFormatType, ProxyUserAgentType
 
 __all__ = [
@@ -8,7 +11,11 @@ __all__ = [
 
 
 class UserPreferencesUpdate(BaseModel):
-    email_gpg_public_key: str = None
+    email_gpg_public_key: str = Field(
+        None,
+        regex=constants.PUBLIC_KEY_REGEX,
+        max_length=constants.PUBLIC_KEY_MAX_LENGTH,
+    )
     alias_remove_trackers: bool = None
     alias_create_mail_report: bool = None
     alias_proxy_images: bool = None
@@ -31,3 +38,23 @@ class UserPreferencesUpdate(BaseModel):
             raise ValueError("You must set at least one preference to update.")
 
         return values
+
+    @validator("email_gp_public_key")
+    def validate_email_gpg_public_key(cls, value: Optional[str]) -> Optional[str]:
+        if not value:
+            return
+
+        value = value.strip()
+        message = f"PGP verification. Your public key is: {value}"
+
+        try:
+            result = gpg_handler.encrypt_message(message, value)
+
+            if not result.ok:
+                raise ValueError(
+                    "This is not a valid PGP public key; we could not encrypt a test message."
+                )
+        except (ValueError, TypeError):
+            raise ValueError(
+                "This is not a valid PGP public key; we could not encrypt a test message."
+            )
